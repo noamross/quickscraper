6d5318874b21364865ee0e11464ed7563773c8ec
Noam Ross <noam.ross@gmail.com>
2014-07-05 17:32:29 -0700

    Basic scraper wrapper

---
+++ R/scrape.R
@@ -0,0 +1,75 @@
+#' @include node.R
+
+#' @export
+qs_cmd = node_fn_load(node_package="quickscrape", r_package="quickscraper")
+
+
+#' @import stringi
+get_package_scrapers = function() {
+  scrapers_dir = system.file("journal-scrapers", "scrapers",
+                             package="quickscraper")
+  scraper_files = list.files(scrapers_dir, pattern="\\.json")
+  scraper_names = stri_match_first_regex(scraper_files, '.+(?=\\.json)')
+  scraper_files = file.path(scrapers_dir, scraper_files)
+  names(scraper_files) = scraper_names
+  return(scraper_files)
+}
+
+package_scrapers = get_package_scrapers()
+
+#' Scrape data from web pages 
+#' 
+#' Take a vector of URLs and scrape data from the associated web pages, using
+#' \code{quickscrape}
+#' @param urls A vector of URLs to scrape
+#' @param scraper A single scraper to use, or list of scrapers the same length
+#' as \code{urls}.  If \code{NULL}, \code{scrape} will choose scrapers based on
+#' the domains of URLs
+#' @param list The form to return results in, either "list", "data.frame", or
+#' "none" to only retain results as JSON files on disk
+#' @param outdir  The directory to write results to.
+#' @import plyr stringi
+#' @export
+scrape = function(urls, url_file=NULL, ratelimit=3, scraper="generic_open", 
+                  args = list(), outdir=NULL) {
+  if (!is.null(url_file)) {
+      args = c(args, urllist = url_file)
+  } else if (length(urls > 1)) {
+      url_file = tempfile()
+      cat(urls, sep="\n", file=url_file)
+      args = c(args, urllist = url_file)
+  } else {
+      args = c(args, url=urls)
+  }
+  
+  if(any(class(scraper) %in% c("list", "scraper"))) {
+    filename = tempfile(fileext="json")
+    cat(toJSON(scraper), file=filename)
+    scraper_obj = scraper
+    scraper = filename
+  } else if(!file.exists(scraper) & (scraper %in% names(package_scrapers))) {
+    scraper = package_scrapers[scraper]
+    names(scraper) = NULL
+    scraper_obj = fromJSON(scraper)
+  } else if(file.exists(scraper)) {
+    scraper_obj = fromJSON(scraper)
+  } else {
+    stop("Scraper not found")
+  }
+  
+  args = c(args, scraper=scraper)
+  
+  if(is.null(outdir)) outdir=tempdir()
+  args = c(args, output=outdir)
+  
+  run = qs_cmd(args)
+
+  out_dirs = list.files(outdir)
+  output = alply(out_dirs, 1, function(z) {
+    fromJSON(file.path(outdir, z, "results.json"), simplifyWithNames=FALSE)
+  })
+  attr(output, "split_type") = NULL
+  attr(output, "split_labels") = NULL
+  names(output) = out_dirs
+  return(output)
+}
